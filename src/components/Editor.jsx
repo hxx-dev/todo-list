e7545ebdf02f59985775859713c117d4eda8b343
hxx-dev <red05077@gmail.com>
2026-07-11 16:43:10 +0900

    :sparkles:UI: 검색창 및 추가 버튼 UI 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,34 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  height: 48px;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  padding: 15px;
+  border: 1px solid rgb(220, 220, 220);
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  width: 80px;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  background-color: rgb(37, 147, 255);
+  cursor: pointer;
+`;
+
 function Editor() {
-  return <>Editor</>;
+  return (
+    <Container>
+      <Input placeholder="새로운 todo..." />
+      <Button>추가</Button>
+    </Container>
+  );
 }
 
 export default Editor;
