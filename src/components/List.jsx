bebd5fcf803a7cadb1bc8a5f9b23683cb6ca33e3
hxx-dev <hxxchangdev@gmail.com>
2026-07-11 16:50:15 +0900

    :sparkles:UI: 모든 UI 구현
    * :art:Design: 레이아웃 가운데 정렬
    
    * :sparkles:UI: Header UI 구현
    
    * :sparkles:UI: 컨텐츠 추가 input 및 추가 버튼 UI 구현
    
    * :sparkles:UI: 리스트 검색창 및 리스트 목록 레이아웃 구현
    
    * :sparkles:UI: 리스트 목록 UI 구현
    
    ---------
    
    Co-authored-by: hxx-dev <red05077@gmail.com>

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,42 @@
 import styled from "styled-components";
+import ListItem from "./ListItem";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const SearchInput = styled.input`
+  padding: 15px;
+  font-size: 15px;
+  flex: 1;
+  border: none;
+  border-radius: 5px;
+  border-bottom: 1px solid rgb(220, 220, 220);
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid rgb(37, 147, 255);
+  }
+`;
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 function List() {
-  return <>List</>;
+  return (
+    <Container>
+      <h4>Todo List 🔥</h4>
+      <SearchInput placeholder="검색어를 입력하세요" />
+      <ListContainer>
+        <ListItem />
+        <ListItem />
+        <ListItem />
+      </ListContainer>
+    </Container>
+  );
 }
 
 export default List;
