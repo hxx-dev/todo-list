d74956753060e8c2b3353e5dd5f76306a995a1ee
hxx-dev <red05077@gmail.com>
2026-07-11 16:42:02 +0900

    :sparkles:UI: Header UI 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
+const DateText = styled.h1`
+  color: rgb(37, 147, 255);
+`;
+
 function Header() {
-  return <>Header</>;
+  return (
+    <div>
+      <h3>오늘은 📅</h3>
+      <DateText>{new Date().toDateString()}</DateText>
+    </div>
+  );
 }
 
 export default Header;
