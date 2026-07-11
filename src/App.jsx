5e0dc7873cd903e31a27031c240ad09f7e6dc8e1
hxx-dev <red05077@gmail.com>
2026-07-11 16:40:46 +0900

    :art:Design: 레이아웃 가운데 정렬

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import Editor from "./components/Editor";
 import List from "./components/List";
 import styled from "styled-components";
 
+const Container = styled.div`
+  /* 세로로 배치, flex를 사용하면 div태그 가로로 배치됨 이때 방향 colomn 사용 -> 세로로 */
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  /* 가운데로 정렬 */
+  width: 500px;
+  margin: 0 auto;
+`;
+
 function App() {
   return (
-    <>
+    <Container>
       <Header />
       <Editor />
       <List />
-    </>
+    </Container>
   );
 }
 
